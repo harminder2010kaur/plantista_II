c596f0916d263793ec36c19a6e926648d237bfc1
Harminder <harminder2010kaur@gmail.com>
2019-06-18 19:30:16 -0400

    Create netlify link

--- client/src/serverUrl.js
+++ client/src/serverUrl.js
@@ -1,3 +1,3 @@
-const serverUrl = `${process.env.REACT_APP_BACKEND_SERVER || "http://localhost:8080"}/ping`;
+const serverUrl = `${process.env.REACT_APP_BACKEND_SERVER || "http://localhost:8080"}`;
 
 export default serverUrl;
